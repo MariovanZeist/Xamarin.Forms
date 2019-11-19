d5bbebf755e0f1050aca2ac8c0184df009cfe53a
E.Z. Hart <hartez@gmail.com>
2019-11-19 08:50:17 -0700

    Create new automated renderer test runner; add automated renderer tests; Set both EntryRenderer alignments simultaneously (fixes #8137) Add renderer test for 8137 Replace some UI tests with renderer tests

--- Xamarin.Forms.Controls/CoreGallery.cs
+++ Xamarin.Forms.Controls/CoreGallery.cs
@@ -14,6 +14,7 @@ using Xamarin.Forms.Controls.Issues;
 using Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.CarouselViewGalleries;
 using Xamarin.Forms.Controls.GalleryPages.RefreshViewGalleries;
 using Xamarin.Forms.Controls.GalleryPages.SwipeViewGalleries;
+using Xamarin.Forms.Controls.GalleryPages.PlatformTestsGallery;
 
 namespace Xamarin.Forms.Controls
 {
@@ -285,6 +286,7 @@ namespace Xamarin.Forms.Controls
 		}
 
 		List<GalleryPageFactory> _pages = new List<GalleryPageFactory> {
+				new GalleryPageFactory(() => new PlatformTestsConsole(), "Platform Automated Tests"),
 				new GalleryPageFactory(() => new MemoryLeakGallery(), "Memory Leak"),
 				new GalleryPageFactory(() => new Issues.A11yTabIndex(), "Accessibility TabIndex"),
 				new GalleryPageFactory(() => new FontImageSourceGallery(), "Font ImageSource"),
---
+++ Xamarin.Forms.Controls/Tests/PlatformTestRunner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using NUnit.Framework.Api;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace Xamarin.Forms.Controls.Tests
+{
+	public class PlatformTestRunner
+	{
+		readonly ITestListener _testListener = new ControlGalleryTestListener();
+
+		public async Task Run(ITestFilter testFilter = null)
+		{
+			testFilter = testFilter ?? TestFilter.Empty;
+
+			var controls = Assembly.GetExecutingAssembly();
+			Assembly platform = DependencyService.Resolve<IAssemblyRetriever>().GetAssembly();
+
+			var runner = new NUnitTestAssemblyRunner(new DefaultTestAssemblyBuilder());
+
+			await Task.Run(() => runner.Load(controls, new Dictionary<string, object>())).ConfigureAwait(false);
+			await Task.Run(() => runner.Run(_testListener, testFilter));
+
+			await Task.Run(() => runner.Load(platform, new Dictionary<string, object>())).ConfigureAwait(false);
+			await Task.Run(() => runner.Run(_testListener, testFilter));
+		}
+	}
+
+	public interface IAssemblyRetriever 
+	{
+		Assembly GetAssembly();
+	}
+}
